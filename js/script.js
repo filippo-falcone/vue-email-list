f792efa50269191ce77d05af4b7cbaa68e90955c
filippo-falcone <falconefilippo98@hotmail.com>
2024-04-09 14:42:15 +0200

    feat: add bonus

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-            emails: []
+            emails: [],
+            emailsLength: 10
         };
     },
     methods: {
         getEmailFromApi() {
-            for (let i = 0; i < 10; i++) {
+            for (let i = 0; i < this.emailsLength; i++) {
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((response) => {
                     const serverData = response.data;
                     const email = serverData.response;
